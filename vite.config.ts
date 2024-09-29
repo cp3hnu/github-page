f662f7351c8cc668563a6fc867680c798ffe256d
cp3hnu <cp3hnu@gmail.com>
2024-09-29 19:45:45 +0800

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,7 @@
 import { defineConfig } from "vite";
-import path from "node:path";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: "move-index-html",
-      configureServer(server) {
-        server.middlewares.use((req, res, next) => {
-          if (req.url === "/") {
-            req.url = "/index.html";
-          }
-          next();
-        });
-      }
-    }
-  ],
-  build: {
-    rollupOptions: {
-      input: path.resolve(__dirname, "public/index.html")
-    }
-  }
+  plugins: [react()]
 });
